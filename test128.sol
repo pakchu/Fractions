3a7249fe4350a6e4b4ae6ba3e0729731453aa120
pakchu <83414365+pakchu@users.noreply.github.com>
2021-08-09 15:56:58 +0900

    sortingByFixed updated

--- test128.sol
+++ test128.sol
@@ -1,10 +1,9 @@
 pragma solidity ^0.8.0;
 
 import './libraries/FractionTuple128.sol';
-import './libraries/SafeMath128.sol';
 
-contract testing{
-    using SafeMath128 for uint128;
+
+contract testing128{
     
     function _sort(uint128 a, uint128 b) external view returns(uint128, uint128){
         return FractionTuple128.sort(a,b);
@@ -18,6 +17,9 @@ contract testing{
     function _lcm(uint128 a, uint128 b) external view returns(uint128){
         return FractionTuple128.lcm(a,b);
     }
+    function _abbreviatable(FractionTuple128.Tuple memory wantedTuple) external view returns(bool){
+        return FractionTuple128.abbreviatable(wantedTuple);
+    }
     function _abbreviate(FractionTuple128.Tuple memory wantedTuple) external view returns(FractionTuple128.Tuple memory){
         return FractionTuple128.abbreviate(wantedTuple);
     }
@@ -27,8 +29,11 @@ contract testing{
     function _commonDenominator(FractionTuple128.Tuple memory a, FractionTuple128.Tuple memory b) external view returns(FractionTuple128.Tuple memory, FractionTuple128.Tuple memory){
         return FractionTuple128.commonDenominator(a,b);
     }
-    function _biggerTuple(FractionTuple128.Tuple memory big, FractionTuple128.Tuple memory small) external view returns(bool){
-        return FractionTuple128.biggerTuple(big, small);
+    function _isbiggerTuple(FractionTuple128.Tuple memory big, FractionTuple128.Tuple memory small) external view returns(bool){
+        return FractionTuple128.isbiggerTuple(big, small);
+    }
+    function _biggerTuple(FractionTuple128.Tuple memory a, FractionTuple128.Tuple memory b) external view returns(FractionTuple128.Tuple memory){
+        return FractionTuple128.biggerTuple(a, b);
     }
     function _addTuple(FractionTuple128.Tuple memory a, FractionTuple128.Tuple memory b) external view returns(FractionTuple128.Tuple memory){
         return FractionTuple128.addTuple(a,b);
@@ -51,11 +56,18 @@ contract testing{
     function _toFixed(FractionTuple128.Tuple memory a) external view returns(uint256){
         return FractionTuple128.toFixed(a);
     }
+    function sortingByFixed(FractionTuple128.Tuple memory a, FractionTuple128.Tuple memory b) external view returns(uint256, FractionTuple128.Tuple memory) {
+        uint256 _a;
+        uint256 _b;
+        (_a,_b) = FractionTuple128.sort256(FractionTuple128.toFixed(a), FractionTuple128.toFixed(b));
+        FractionTuple128.Tuple memory c;
+        FractionTuple128.Tuple memory d;
+        if (FractionTuple128.isbiggerTuple(a, b)){
+            (c,d) = (a,b);
+        } else {
+            (c,d) = (b,a);
+        }
+        require( _a == FractionTuple128.toFixed(c) && _b == FractionTuple128.toFixed(d),"something went wrong, plz let me know if this message shows");
+        return (_a, c);
+    }
 }
-
-
-
-
-
-
-
