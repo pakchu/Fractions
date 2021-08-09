59f7476f920214a67d95902597b2d24018f85837
pakchu <83414365+pakchu@users.noreply.github.com>
2021-08-09 16:00:02 +0900

    Update test.sol

--- test.sol
+++ test.sol
@@ -1,10 +1,8 @@
 pragma solidity ^0.8.0;
 
 import './libraries/FractionTuple.sol';
-import './libraries/SafeMath.sol';
 
 contract testing{
-    using SafeMath for uint256;
     
     function _sort(uint256 a, uint256 b) external view returns(uint256, uint256){
         return FractionTuple.sort(a,b);
@@ -15,6 +13,9 @@ contract testing{
     function _lcm(uint256 a, uint256 b) external view returns(uint256){
         return FractionTuple.lcm(a,b);
     }
+    function _abbreviatable(FractionTuple.Tuple memory wantedTuple) external view returns(bool){
+        return FractionTuple.abbreviatable(wantedTuple);
+    }
     function _abbreviate(FractionTuple.Tuple memory wantedTuple) external view returns(FractionTuple.Tuple memory){
         return FractionTuple.abbreviate(wantedTuple);
     }
@@ -24,14 +25,17 @@ contract testing{
     function _commonDenominator(FractionTuple.Tuple memory a, FractionTuple.Tuple memory b) external view returns(FractionTuple.Tuple memory, FractionTuple.Tuple memory){
         return FractionTuple.commonDenominator(a,b);
     }
-    function _biggerTuple(FractionTuple.Tuple memory big, FractionTuple.Tuple memory small) external view returns(bool){
-        return FractionTuple.biggerTuple(big, small);
+    function _isbiggerTuple(FractionTuple.Tuple memory big, FractionTuple.Tuple memory small) external view returns(bool){
+        return FractionTuple.isbiggerTuple(big, small);
+    }
+    function _biggerTuple(FractionTuple.Tuple memory a, FractionTuple.Tuple memory b) external view returns(FractionTuple.Tuple memory){
+        return FractionTuple.biggerTuple(a, b);
     }
     function _addTuple(FractionTuple.Tuple memory a, FractionTuple.Tuple memory b) external view returns(FractionTuple.Tuple memory){
         return FractionTuple.addTuple(a,b);
     }
-    function _subtractTuple(FractionTuple.Tuple memory a, FractionTuple.Tuple memory b) external view returns(FractionTuple.Tuple memory){
-        return FractionTuple.subtractTuple(a,b);
+    function _subtractTuple(FractionTuple.Tuple memory a, FractionTuple.Tuple memory subtractingTuple) external view returns(FractionTuple.Tuple memory){
+        return FractionTuple.subtractTuple(a,subtractingTuple);
     }
     function _simpleMultiplyTuple(FractionTuple.Tuple memory a, FractionTuple.Tuple memory b) external view returns(FractionTuple.Tuple memory){
         return FractionTuple.simpleMultiplyTuple(a,b);
@@ -46,10 +50,3 @@ contract testing{
         return FractionTuple.simpleDivideTuple(a, dividingTuple);
     }
 }
-
-
-
-
-
-
-
