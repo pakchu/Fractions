ddb6aedeb3e21a02898c8fda949443c3c24e5d40
pakchu <83414365+pakchu@users.noreply.github.com>
2021-08-09 16:18:19 +0900

    Update test128.sol

--- test128.sol
+++ test128.sol
@@ -2,9 +2,7 @@ pragma solidity ^0.8.0;
 
 import './libraries/FractionTuple128.sol';
 
-
-contract testing128{
-    
+contract testing128{    
     function _sort(uint128 a, uint128 b) external view returns(uint128, uint128){
         return FractionTuple128.sort(a,b);
     }
@@ -38,8 +36,8 @@ contract testing128{
     function _addTuple(FractionTuple128.Tuple memory a, FractionTuple128.Tuple memory b) external view returns(FractionTuple128.Tuple memory){
         return FractionTuple128.addTuple(a,b);
     }
-    function _subtractTuple(FractionTuple128.Tuple memory a, FractionTuple128.Tuple memory b) external view returns(FractionTuple128.Tuple memory){
-        return FractionTuple128.subtractTuple(a,b);
+    function _subtractTuple(FractionTuple128.Tuple memory a, FractionTuple128.Tuple memory subtractingTuple) external view returns(FractionTuple128.Tuple memory){
+        return FractionTuple128.subtractTuple(a,subtractingTuple);
     }
     function _simpleMultiplyTuple(FractionTuple128.Tuple memory a, FractionTuple128.Tuple memory b) external view returns(FractionTuple128.Tuple memory){
         return FractionTuple128.simpleMultiplyTuple(a,b);
