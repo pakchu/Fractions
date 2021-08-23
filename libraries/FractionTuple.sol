b57121c6d887100c74e18fe8640e1fb51282d01f
pakchu <gus4734@gmail.com>
2021-08-23 16:14:54 +0900

    Update FractionTuple.sol
    functions, arguments name changes

--- libraries/FractionTuple.sol
+++ libraries/FractionTuple.sol
@@ -2,10 +2,10 @@ pragma solidity ^ 0.8.0;
 
 import './SafeMath.sol';
 
-library FractionTuple {
+library fractionLib {
     using SafeMath for uint256;
-    // defining tuple for fraction expression
-    struct Tuple {
+    // defining a strtuct for fraction expression
+    struct Fraction {
         uint256 numerator;
         uint256 denominator;
     }
@@ -17,6 +17,7 @@ library FractionTuple {
     }
     // function for getting greatest common diviosor which is pretty useful operating fractions
     // using Euclidean Algoithm, you can find info's here: https://en.wikipedia.org/wiki/Euclidean_algorithm
+    // it has time complexity of: O(log(min(a,b))) = O(log(n)), and nice proof for this: https://www.geeksforgeeks.org/time-complexity-of-euclidean-algorithm/
     function gcd(uint256 a, uint256 b) internal pure returns(uint256){
         (a,b) = sort(a,b);
         while(b != 0 ) {
@@ -29,106 +30,98 @@ library FractionTuple {
     function lcm(uint256 a, uint256 b) internal pure returns(uint256){
         return (a / gcd(a,b) ).mul(b);
     }
-    // check if a tuple's denominator is 0
-    modifier zeroDivide(Tuple memory tuple) {
-        require (tuple.denominator != 0, "denominator cannot be 0");
+    // check if a Fraction's denominator is 0
+    modifier denominatorIsNotZero(Fraction memory frac) {
+        require (frac.denominator != 0, "denominator cannot be 0");
         _;
     }
-    // is tuple a reducible fraction 
-    function abbreviatable(Tuple memory wantedTuple) internal pure zeroDivide(wantedTuple) returns(bool) {
-        if(gcd(wantedTuple.numerator,wantedTuple.denominator) != 1) return true;
+    // is Fraction a reducible fraction 
+    function isAbbreviatable(Fraction memory frac) internal pure denominatorIsNotZero(frac) returns(bool) {
+        if(gcd(frac.numerator,frac.denominator) != 1) return true;
         else return false;
     }
-    // making tuple into reduced fraction
-    function abbreviate(Tuple memory wantedTuple)internal pure zeroDivide(wantedTuple) returns (Tuple memory){
-        if (abbreviatable(wantedTuple)) {
-            uint256 _gcd = gcd(wantedTuple.numerator, wantedTuple.denominator);
-            return Tuple(wantedTuple.numerator / _gcd, wantedTuple.denominator / _gcd);
-        } else return wantedTuple;
+    // making Fraction into reduced fraction
+    function abbreviate(Fraction memory frac)internal pure denominatorIsNotZero(frac) returns (Fraction memory){
+        if (isAbbreviatable(frac)) {
+            uint256 _gcd = gcd(frac.numerator, frac.denominator);
+            return Fraction(frac.numerator / _gcd, frac.denominator / _gcd);
+        } else return frac;
     }
     
-    function reverseTuple(Tuple memory wantedTuple) internal pure zeroDivide(wantedTuple) returns(Tuple memory result){
-        require(wantedTuple.numerator != 0, "denominator cannot be 0");
-        result = Tuple( wantedTuple.denominator, wantedTuple.numerator );
+    function toReciprocal(Fraction memory frac) internal pure denominatorIsNotZero(frac) returns(Fraction memory){
+        require(frac.numerator != 0, "denominator cannot be 0");
+        return Fraction( frac.denominator, frac.numerator );
     }
-    // finding lcm of denominators of two tuples
-    function commonDenominator(Tuple memory a, Tuple memory b) internal pure zeroDivide(a) zeroDivide(b) returns(Tuple memory, Tuple memory){
+    // finding lcm of denominators of two Fractions
+    function reduceToCommonDenomiantor(Fraction memory a, Fraction memory b) internal pure denominatorIsNotZero(a) denominatorIsNotZero(b) returns(Fraction memory, Fraction memory){
         uint256 lcm = lcm(a.denominator, b.denominator);
         uint256 numerator0 = (lcm / a.denominator).mul(a.numerator);
         uint256 numerator1 = (lcm / b.denominator).mul(b.numerator);
-        return (Tuple(numerator0, lcm), Tuple(numerator1, lcm));
+        return (Fraction(numerator0, lcm), Fraction(numerator1, lcm));
     }
     
-    function isbiggerTuple(Tuple memory big, Tuple memory small)internal pure zeroDivide(big) zeroDivide(small) returns(bool) {
-        Tuple memory c;
-        Tuple memory d;
-        (c, d) = commonDenominator(big, small);
+    function isGreaterThan(Fraction memory big, Fraction memory small)internal pure denominatorIsNotZero(big) denominatorIsNotZero(small) returns(bool) {
+        Fraction memory c;
+        Fraction memory d;
+        (c, d) = reduceToCommonDenomiantor(big, small);
         if (c.numerator >= d.numerator) return true;
         else return false;
     }
 
-    function biggerTuple(Tuple memory a, Tuple memory b)internal pure zeroDivide(a) zeroDivide(b) returns(Tuple memory) {
-        Tuple memory c;
-        Tuple memory d;
-        (c, d) = commonDenominator(a, b);
-        if (c.numerator >= d.numerator) return a;
-        else return b;
-    }
-    // for some cases which do not need abbreviation
-    function simpleAddTuple(Tuple memory a, Tuple memory b) internal pure zeroDivide(a) zeroDivide(b) returns(Tuple memory c){
-        (a,b) = commonDenominator(a,b);
-        c = Tuple( a.numerator.add(b.numerator) , a.denominator );
+    // for some uses which do not need abbreviation
+    function _add(Fraction memory a, Fraction memory b) internal pure denominatorIsNotZero(a) denominatorIsNotZero(b) returns(Fraction memory c){
+        (a,b) = reduceToCommonDenomiantor(a,b);
+        c = Fraction( a.numerator.add(b.numerator) , a.denominator );
     }
     
-    function addTuple(Tuple memory a, Tuple memory b) internal pure zeroDivide(a) zeroDivide(b) returns(Tuple memory c){
-        c = abbreviate(simpleAddTuple(a,b));
+    function add(Fraction memory a, Fraction memory b) internal pure denominatorIsNotZero(a) denominatorIsNotZero(b) returns(Fraction memory c){
+        c = abbreviate(_add(a,b));
     }
-    // for some cases which do not need abbreviation
-    function simpleSubtractTuple(Tuple memory a, Tuple memory subtractingTuple) internal pure zeroDivide(a) zeroDivide(subtractingTuple) returns(Tuple memory c){
-        require( isbiggerTuple(a, subtractingTuple), "Tuple: Underflow" );
-        (a,subtractingTuple) = commonDenominator(a,subtractingTuple);
-        c = Tuple( a.numerator.sub(subtractingTuple.numerator) , a.denominator );
+    // for some uses which do not need abbreviation
+    function _sub(Fraction memory a, Fraction memory b) internal pure denominatorIsNotZero(a) denominatorIsNotZero(b) returns(Fraction memory c){
+        require( isGreaterThan(a, b), "Fraction: Underflow" );
+        (a,b) = reduceToCommonDenomiantor(a,b);
+        c = Fraction( a.numerator.sub(b.numerator) , a.denominator );
     }
 
-    function subtractTuple(Tuple memory a, Tuple memory subtractingTuple) internal pure zeroDivide(a) zeroDivide(subtractingTuple) returns(Tuple memory c){
-        c = abbreviate(simpleSubtractTuple(a,subtractingTuple));
+    function sub(Fraction memory a, Fraction memory b) internal pure denominatorIsNotZero(a) denominatorIsNotZero(b) returns(Fraction memory c){
+        c = abbreviate(_sub(a,b));
+    }
+    // for some uses which do not need abbreviation
+    function _mul(Fraction memory a, Fraction memory b)internal pure denominatorIsNotZero(a) denominatorIsNotZero(b) returns(Fraction memory c){
+        c = Fraction(a.numerator.mul(b.numerator) , a.denominator.mul(b.denominator));
     }
     
-    function multiplyTuple(Tuple memory a, Tuple memory b) internal pure zeroDivide(a) zeroDivide(b) returns(Tuple memory result) {
+    function mul(Fraction memory a, Fraction memory b) internal pure denominatorIsNotZero(a) denominatorIsNotZero(b) returns(Fraction memory result) {
         a = abbreviate(a);
         b = abbreviate(b);
-        Tuple memory c = abbreviate(Tuple( a.numerator , b.denominator));
-        Tuple memory d = abbreviate(Tuple( b.numerator , a.denominator));
-        result = Tuple( c.numerator.mul(d.numerator) , c.denominator.mul(d.denominator));
+        Fraction memory c = abbreviate(Fraction( a.numerator , b.denominator));
+        Fraction memory d = abbreviate(Fraction( b.numerator , a.denominator));
+        result = Fraction( c.numerator.mul(d.numerator) , c.denominator.mul(d.denominator));
     }
-    // for some cases which do not need abbreviation
-    function simpleMultiplyTuple(Tuple memory a, Tuple memory b)internal pure zeroDivide(a) zeroDivide(b) returns(Tuple memory c){
-        c = Tuple(a.numerator.mul(b.numerator) , a.denominator.mul(b.denominator));
+    // for some uses which do not need abbreviation
+    function _div(Fraction memory a, Fraction memory b)internal pure denominatorIsNotZero(a) denominatorIsNotZero(b) returns(Fraction memory c) {
+        require( b.numerator != 0, "denominator cannot be 0");
+        c = _mul(a, toReciprocal(b));
     }
-
-    function divideTuple(Tuple memory a, Tuple memory dividingTuple) zeroDivide(a) zeroDivide(dividingTuple) internal pure returns(Tuple memory result){
-        require(dividingTuple.numerator != 0, "denominator cannot be 0");
-        result = multiplyTuple(a, reverseTuple(dividingTuple));
+    function div(Fraction memory a, Fraction memory b) denominatorIsNotZero(a) denominatorIsNotZero(b) internal pure returns(Fraction memory result){
+        require(b.numerator != 0, "denominator cannot be 0");
+        result = mul(a, toReciprocal(b));
     }
-    // for some cases which do not need abbreviation
-    function simpleDivideTuple(Tuple memory a, Tuple memory dividingTuple)internal pure zeroDivide(a) zeroDivide(dividingTuple) returns(Tuple memory c) {
-        require( dividingTuple.numerator != 0, "denominator cannot be 0");
-        c = simpleMultiplyTuple(a, reverseTuple(dividingTuple));
-    }
-    // by adjusting divider, it can help the tuple not to overflow.
+    // by adjusting divider, it can help the Fraction not to overflow.
     // since dividing both numerator and denomiantor with same number keeps the original value of fraction,
     // but as in solidity which is only able to express integers not floats, integers divided by some integers give out approximated value
-    function antiOverflow(Tuple memory a, uint256 divider) internal pure zeroDivide(a) returns(Tuple memory){
-        return Tuple(a.numerator / divider, a.denominator / divider);
+    function preventOverflow(Fraction memory frac, uint256 divider) internal pure denominatorIsNotZero(frac) returns(Fraction memory){
+        return Fraction(frac.numerator / divider, frac.denominator / divider);
     }
-    function fractionToMixed(Tuple memory a) internal pure zeroDivide(a) returns(uint256 integer, Tuple memory fraction){
+    function fractionToMixed(Fraction memory a) internal pure denominatorIsNotZero(a) returns(uint256 integer, Fraction memory fraction){
         integer = a.numerator / a.denominator;
-        fraction = abbreviate(Tuple(a.numerator.sub((integer.mul(a.denominator))), a.denominator));
+        fraction = abbreviate(Fraction(a.numerator.sub((integer.mul(a.denominator))), a.denominator));
     }
-    // by giving an uint and [0,1] to this function, it can also work as translator uint -> tuple
+    // by giving an uint and [0,1] to this function, it can also work as translator uint -> Fraction
     // therefore, using mixedToFraction() -> any computing functions given -> fractionToMixed() -> (result in uint, ) 
-    // and the result uint can be more precise than just using uint computations, since uint's computation always round down more than tuple's round down    
-    function mixedToFraction(uint256 integer, Tuple memory fraction) internal pure zeroDivide(fraction) returns(Tuple memory result){
-        result = abbreviate(Tuple(fraction.numerator.add( integer.mul(fraction.denominator)), fraction.denominator));
+    // and the result uint can be more precise than just using uint computations, since uint's computation always round down more than Fraction's round down    
+    function mixedToFraction(uint256 integer, Fraction memory fraction) internal pure denominatorIsNotZero(fraction) returns(Fraction memory result){
+        result = abbreviate(Fraction(fraction.numerator.add( integer.mul(fraction.denominator)), fraction.denominator));
     }
 }
