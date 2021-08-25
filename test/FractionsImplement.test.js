b11a5aaf90eb41f9c8d5f19d36cfaf3ddf61d8aa
pakchu <gus4734@gmail.com>
2021-08-25 15:36:18 +0900

    test

---
+++ test/FractionsImplement.test.js
@@ -0,0 +1,9 @@
+const {
+    BN,           // Big Number support
+    constants,    // Common constants, like the zero address and largest integers
+    expectEvent,  // Assertions for emitted events
+    expectRevert, // Assertions for transactions that should fail
+} = require('@openzeppelin/test-helpers');
+
+const FractionsImplement = artifacts.require('FractionsImplement');
+
